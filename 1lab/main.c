427a52c1d59fe9e76f1c134e2adb1d986190da89
dashkamalkina <dashamalkina01@icloud.com>
2021-11-11 15:47:57 +0300

    inited

---
+++ 1lab/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char c;
+    float a;
+
+
+    scanf("%c", &c);
+    printf("%c\n", c);
+
+    scanf("%f", &a);
+    printf("%f\n", a);
+    return 0;
+}
