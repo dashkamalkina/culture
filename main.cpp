4fea5921089e377e801e1c35126e44db4e7d9f9b
dashkamalkina <91782000+dashkamalkina@users.noreply.github.com>
2022-06-04 21:03:25 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,83 +1,33 @@
 #include <iostream>
-#include <vector>
+#include "cube.h"
 
-namespace my {
-    template<class ForwardIterator, class Pred>
-    bool all_of (ForwardIterator first, ForwardIterator last, Pred pred)
+int main()
+{
+    freopen("input.in","r",stdin);
+//    freopen("output.out","w",stdout);
+
+    cout << "Solving Rubik's Cube" << endl;
+    cout<<"white: 1 orange: 2 green: 3 red: 4 blue: 5 yellow: 6 " << endl;
+
+    CubeSolver solver;
+    vector<vector<int>> tmp;
+    for (auto v:tmp) {
+        tmp.resize(3);
+    }
+
+    for(int i=0;i<6;i++)
     {
-        while (first!=last) {
-            if (!pred(*first)) {
-                return false;
-            }
-            first++;
-        }
-        return true;
+        for(int ii=0; ii < 3; ii++)
+            for(int j=0;j<3;j++) cin>>tmp[ii][j];
+
+        solver.enter_face(tmp);
     }
 
-    template <class ForwardIterator, class Pred>
-    bool is_partitioned (ForwardIterator first, ForwardIterator last, Pred pred)
-    {
-        while (first!=last && pred(*first)) {
-            first++;
-        }
-        while (first!=last) {
-            if (pred(*first)) {
-                return false;
-            }
-            first++;
-        }
-        return true;
+    if(solver.is_cube_wrong()) {
+        cout << "Can't solve";
+    } else {
+        solver.solve();
     }
 
-    template<class BiDirIterator, class T>
-    BiDirIterator find_backward (BiDirIterator first, BiDirIterator last, const T& val)
-    {
-        while (first!=last) {
-            if (*last==val) return last;
-            --last;
-        }
-        return --last;
-    }
-}
-
-bool is_0(int a) {
-    return a == 0;
-}
-
-bool is_even(int i) {
-    return i % 2 == 0;
-}
-
-class CPoint {
-public:
-    int x,y;
-    CPoint(int x, int y) {
-        this->x = x;
-        this->y = y;
-    }
-};
-
-bool is_xy_sum_odd(CPoint p) {
-    return (p.x + p.y)%2 != 0;
-}
-
-int main() {
-    std::vector a = {1,2,2,2,2,2,4,4,4,4,2};
-    std::cout << "array size :"<< a.size() << std::endl;
-    for (auto i : a) {
-        std::cout << i << " ";
-    }
-
-    std::cout << std::boolalpha;
-    std::cout << std::endl << "Are all of elements are 0" << std::endl;
-    std::cout << my::all_of(a.begin(), a.end(), is_0);
-    std::cout << std::endl << "Are elements partitioned by is_even" << std::endl;
-    std::cout << my::is_partitioned(a.begin(), a.end(), is_even);
-    std::cout << std::endl << "Find first index from tail which equals 2" << std::endl;
-    std::cout << my::find_backward(a.begin(), a.end(), 2) - a.begin();
-
-    std::vector b = {CPoint(1,2), CPoint(2,3), CPoint(3,4)};
-    std::cout << std::endl << "Are all of elements are is_xy_sum_odd" << std::endl;
-    std::cout << my::all_of(b.begin(), b.end(), is_xy_sum_odd) << std::endl;
     return 0;
 }
